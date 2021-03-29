5da5536ac95d949945e6380f8da835308381d3ab
Luis Gomez <lumgomezca@unal.edu.co>
2021-03-28 19:08:14 -0500

    Segundo Commit Luis

---
+++ src/Ajedrez.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Ajedrez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ingrese nombre de jugador 1 (Fichas blancas):");
+    }
+    
+}
